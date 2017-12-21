9e3f4b56a5f998f85c9c04c35f9eb7759c9ec4a5
DakHarry <monkey030210@gmail.com>
2017-12-22 01:29:28 +0800

    revised sw register

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,12 +1,20 @@
 var deferredPrompt;
 
 if('serviceWorker' in navigator){
-    navigator.serviceWorker
-        .register('/service-worker.js')
+    // navigator.serviceWorker
+    //     .register('/service-worker.js')
+    //     .then(function(){
+    //         console.log('Service Worker 註冊成功');
+    //     });
+    window.addEventListener('load', function(){
+        navigator.serviceWorker.register('/service-worker.js')
         .then(function(){
             console.log('Service Worker 註冊成功');
         });
+    });
 }
+
+
 
 window.addEventListener('beforeinstallprompt', function(event){
     console.log('beforeinstallprompt fired');
